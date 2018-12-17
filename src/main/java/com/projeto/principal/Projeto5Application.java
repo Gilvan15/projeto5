fdde903e425281f961df1ff479a48d93ffbba051
Gilvan <gilvan.nascimento@institutoitn.com.br>
2018-12-17 14:52:01 -0400

    commit inical

---
+++ src/main/java/com/projeto/principal/Projeto5Application.java
@@ -0,0 +1,14 @@
+package com.projeto.principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto5Application.class, args);
+	}
+
+}
+
